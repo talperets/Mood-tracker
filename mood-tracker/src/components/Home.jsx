28786137ff12883293868171fcfb9aa727c6f405
talperets <tal.perets3@gmail.com>
2023-04-28 00:41:00 +0300

    home component & css

--- mood-tracker/src/components/Home.jsx
+++ mood-tracker/src/components/Home.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import "../style.css";
 export default function Home() {
+  const feelings = [
+    "happy",
+    "sad",
+    "angry",
+    "excited",
+    "nervous",
+    "calm",
+    "bored",
+    "anxious",
+    "confused",
+  ];
+  const toCapital = (string) => {
+    const firstLetter = string.charAt(0).toUpperCase();
+    const rest = string.slice(1);
+    return firstLetter + rest;
+  };
   return (
     <>
-      <h2>How do you feel?</h2>
+      <h2 className="title">How do you feel?</h2>
 
       <div class="btn-container">
-        <button id="happy">Happy</button>
-        <button id="sad">Sad</button>
-        <button id="angry">Angry</button>
-        <button id="excited">Excited</button>
-        <button id="nervous">Nervous</button>
-        <button id="calm">Calm</button>
-        <button id="bored">Bored</button>
-        <button id="anxious">Anxious</button>
-        <button id="confused">Confused</button>
+        {feelings.map((val) => {
+          return <button id={val}>{toCapital(val)}</button>;
+        })}
       </div>
     </>
   );
